d9148ce9e179169db5813dc9971bdb71253a3151
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-05-03 22:54:53 +0500

    ADD

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const { rows } = require("../util/database")
+
+const topicsSQL = `
+select * from topics
+`
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+
+  const topics = await rows(topicsSQL)
+
+  res.send(topics);
 });
 
 module.exports = router;
---
+++ util/database.js
@@ -0,0 +1,29 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  host: 'localhost',
+  user: 'muhammadbobur',
+  password: '1111',
+  database: 'platforma',
+  port: 5432
+})
+
+const rows = async (SQL, ...params) => {
+
+  const connect = await pool.connect()
+
+  try {
+    const { rows } = await connect.query(SQL, params)
+
+    return rows
+  }
+  catch (e) {
+    throw e
+  }
+  finally {
+    connect.release()
+  }
+
+}
+
+module.exports.rows = rows
