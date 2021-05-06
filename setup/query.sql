a25a884760e4447b39abcb6bc32b6e10afa72811
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-05-06 13:20:45 +0500

    Add new query

--- setup/query.sql
+++ setup/query.sql
@@ -1,38 +1,20 @@
--- author and topic 
 
-select
-  ARRAY_AGG(a.author_lastname),
-  ARRAY_AGG(a.author_firstname),
-  ARRAY_AGG(t.topic_id),
-  ARRAY_AGG(t.topic_title),
-  ARRAY_AGG(t.topic_summary),
-  l.lecture_id
-from
-  topics as t
-join
-  authors as a on a.author_id = t.author_id
-join
-  lectures as l on l.topic_id = t.topic_id
-group by
-  l.lecture_id
-;
 
+
+
+--ishladi ALhamdulillah--
 
 select
   a.author_lastname,
-  a.author_firstname,
-  t.topic_id,
   t.topic_title,
-  t.topic_summary
+  array_agg(l.lecture_title) as lactures
 from
-  topics as t
+  authors as a
 join
-  authors as a on a.author_id = t.author_id
-join
+  topics as t on t.author_id = a.author_id
+left join
   lectures as l on l.topic_id = t.topic_id
 group by
-  author_lastname,
-  author_firstname
+  a.author_lastname,
+  t.topic_title
 ;
-
-
