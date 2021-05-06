bc4b6111cacc77fbb63ac64513f9fcd0efca6525
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-05-06 13:25:41 +0500

    Add lactures query

--- setup/query.sql
+++ setup/query.sql
@@ -18,3 +18,20 @@ group by
   a.author_lastname,
   t.topic_title
 ;
+
+--bu keyingi query yani maruzalar uchun--
+select
+  a.author_lastname,
+  t.topic_title,
+  array_agg(l.lecture_title) as lactures
+from
+  authors as a
+join
+  topics as t on t.author_id = a.author_id and t.topic_id = 2
+left join
+  lectures as l on l.topic_id = t.topic_id
+group by
+  a.author_lastname,
+  t.topic_title
+;
+
